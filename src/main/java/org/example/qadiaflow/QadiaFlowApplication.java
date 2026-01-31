a315ce7e68b2b1ed3178f39c49bb979f9cded92d
mohamedsayedms4 <mohamedms4002@gmail.com>
2026-01-31 05:04:41 +0200

    chore(init): bootstrap Spring Boot project structure

---
+++ src/main/java/org/example/qadiaflow/QadiaFlowApplication.java
@@ -0,0 +1,13 @@
+package org.example.qadiaflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QadiaFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QadiaFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/qadiaflow/QadiaFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.qadiaflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QadiaFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
